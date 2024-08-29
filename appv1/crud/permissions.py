2427a0c5663ca51b1cb1239e52f2ac177ff922ee
Luisa Agudelo <luisaab1103@gmail.com>
2024-08-29 13:29:58 -0500

    Modificaciones

--- appv1/crud/permissions.py
+++ appv1/crud/permissions.py
@@ -15,13 +15,13 @@ def get_permissions(db: Session, rol: str, module: str):
         raise HTTPException(status_code=500, detail="Error al buscar usuario por email")
     
 
-# Consultar  todos los permisos de un rol por modulo
+# consultar todos los permisos de un rol
 def get_all_permissions(db: Session, rol: str):
     try:
-        sql = text("SELECT p_select, module_name FROM permissions WHERE rol_name = :rol")
-        result = db.execute(sql, {"rol": rol}).fetchall()
+        sql = text("SELECT module_name, p_select FROM permissions WHERE rol_name = :rol")
+        result = db.execute(sql, {"rol": rol}).mappings().all()
         return result
-    except  SQLAlchemyError as e:
-        print(f"Error al buscar usuario por : {e}")
-        raise HTTPException(status_code=500, detail="Error al buscar usuario por email")
+    except SQLAlchemyError as e:
+        print(f"Error al obtener permisos: {e}")
+        raise HTTPException(status_code=500, detail="Error al obtener permisos")
     
--- appv1/routers/login.py
+++ appv1/routers/login.py
@@ -1,9 +1,9 @@
 from typing import Annotated, List
 from appv1.crud.permissions import get_all_permissions
-from appv1.schemas.user import ResponseLoggin, UserLoggin
+from appv1.schemas.user import ResponseLoggin, UserCreate, UserLoggin
 from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
-from appv1.crud.users import get_user_by_email, get_user_by_id
+from appv1.crud.users import get_user_by_email, get_user_by_id,create_user_sql
 from core.security import create_access_token, verify_password,verify_token
 from db.database import get_db
 from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
@@ -79,4 +79,44 @@ async def login_for_access_token(
         permissions=permisos,
         access_token= access_token
     )
+
+@router.post("/token", response_model=ResponseLoggin)
+async def login_for_access_token(
+    form_data: Annotated[OAuth2PasswordRequestForm, Depends()],
+    db: Session = Depends(get_db)
+):
+    user = authenticate_user(form_data.username, form_data.password, db)
+    if not user:
+        raise HTTPException(
+            status_code=401,
+            detail="Datos Incorrectos en email o password",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+    access_token = create_access_token(
+        data={"sub": user.user_id, "rol":user.user_role}
+    )
+
+    permisos = get_all_permissions(db, user.user_role)
+
+    return ResponseLoggin(
+        user=UserLoggin(
+            user_id=user.user_id,
+            full_name=user.full_name,
+            mail=user.mail,
+            user_role=user.user_role
+        ),
+        permissions=permisos,
+        access_token=access_token
+    )
+
+@router.post("/register")
+async def register_user(
+    user: UserCreate,
+    db: Session = Depends(get_db)
+):
+    user.user_role = 'Cliente'
+    respuesta = create_user_sql(db, user)
+    if respuesta:
+        return {"mensaje":"usuario registrado con éxito"}
+    
 
